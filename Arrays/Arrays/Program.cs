c081dde982f106c94c4aa5a1f04a5814e35f6961
Katie Duane <katiejduane@gmail.com>
2020-06-14 22:41:04 -0400

    strings and arrayz 2

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+// declaring an array: int[] numbers = new int[3];
+// you must declare length of array when you create it, and allocate memory by using the 'new' keyword. it an obejct behind the scene!
+// this is why we need to allocate memory for it! :)
+// in C# arrays ARE 0 indexed
+// if you know what the elements of each array index, you can add them all at once instead of numbers[0] = 1; by using:
+// int[] numbers = new int[3]{1,2,3};
+// if you do not declare an index, it will default to 0 (for ints), for booleans, defaults to false
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "bobby", "jack", "mary" };
+            Console.WriteLine(names[0]);
+        }
+    }
+}
---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+// many ways to devlar strings, most common is: string firstName = "Eitak";
+// there is also something called string concatenation! using string.Format()
+// you can also  create strings by joining arrays
+// like arrays, you can access indices of string, like name[0] or whatever
+// in C# strings are IMMUTABLE! so if name[0] == "E", we cannot changed it by swriting name[0] = "M";
+
+/* 
+
+escape chars for strings -->
+\n = new line
+\t = tab
+\\ = backslash
+\' = single quote
+\" = doubele quote
+
+Verbatim String --> (helpful when you want or need to use special chars) prefix with @, as in:
+string path = @"c:\projects\project1\folder1";
+
+string name = "Katie" --> C#
+String name = "Katie" --> .NET framework String class from the System namespace, so if you want to do this, must put using System; at top of file
+*/
+
+
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstname = "Eitak";
+            var lastname = "Enaud";
+            string myName = string.Format("My name is {0} {1}", firstname, lastname); // concatenation
+            Console.WriteLine(myName);
+
+            var numbers = new int[3] { 1, 2, 3 };
+            var list = string.Join(",", numbers);
+            Console.WriteLine(list);
+
+            var names = new string[3] { "jack", "jon", "milly" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text = @"Hi John
+Look into the following paths for the file:
+c:\projects\folder1
+c:\projects\folder2";
+            Console.WriteLine(text);
+
+        }
+    }
+}
